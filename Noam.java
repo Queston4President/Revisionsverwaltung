f9a178040d206c725778e1dd35c7643a425023e0
Noam <noam.breunig@web.de>
2023-05-24 08:28:27 +0200

    Erstellen eines neuen Schuelers Noam

---
+++ Noam.java
@@ -0,0 +1,17 @@
+public class Noam extends Schueler
+{
+    private String Name;
+    private int Alter;
+
+    public Noam()
+    {
+        Name = "Noam";
+        Alter = 16;
+    }
+    
+    public void GibInfo()
+    {
+        System.out.print("Meine Name ist " + Name +". ");
+        System.out.println("Ich bin " + Alter + " Jahre alt.");
+    }
+}
